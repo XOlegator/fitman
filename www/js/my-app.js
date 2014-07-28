0d224252ddd0939cbb05529bacc628d6049a4289
XOlegator <subspam@mail.ru>
2014-07-28 14:15:41 +0400

    Bootstrap заменён на Framework 7. Реализован макет слайдов по 14-й включительно.

---
+++ www/js/my-app.js
@@ -0,0 +1,65 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $$ = Framework7.$;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+// Callbacks to run specific code for specific pages, for example for About page:
+myApp.onPageInit('about', function (page) {
+    // run createContentPage func after link was clicked
+    $$('.create-page').on('click', function () {
+        createContentPage();
+    });
+});
+
+// Generate dynamic page
+var dynamicPageIndex = 0;
+function createContentPage() {
+	mainView.loadContent(
+        '<!-- Top Navbar-->' +
+        '<div class="navbar">' +
+        '  <div class="navbar-inner">' +
+        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back-blue"></i><span>Back</span></a></div>' +
+        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+        '  </div>' +
+        '</div>' +
+        '<div class="pages">' +
+        '  <!-- Page, data-page contains page name-->' +
+        '  <div data-page="dynamic-pages" class="page">' +
+        '    <!-- Scrollable page content-->' +
+        '    <div class="page-content">' +
+        '      <div class="content-block">' +
+        '        <div class="content-block-inner">' +
+        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+        '        </div>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>'
+    );
+	return;
+}
+// Управляем видимостью кнопок Delete в настройках упражнений
+$$('.btn-delete-toggle').on('change', function(e) {
+	var collapse_content_selector = '#' + $$(this).attr('name');
+	$$(collapse_content_selector).toggleClass('hidden');
+});
+// Модальное окно для подтверждения удаления клиентов
+$$('.confirm-ok-cancel').on('click', function () {
+    myApp.confirm('Are you sure?', 
+      function () {
+        myApp.alert('You clicked Ok button');
+      },
+      function () {
+        myApp.alert('You clicked Cancel button');
+      }
+    );
+});
+
