d1b283b63ef6d1e85786336894b379e041a2ffa7
XOlegator <subspam@mail.ru>
2014-07-30 17:42:38 +0400

    Осваивается работа с базой данных. В индексном файле есть закомментированный код, который заполняет локальную базу данных данными из json файлов. Страница Customers подгружает список клиентов из локальной базы данных.

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,51 +1,107 @@
 // Initialize your app
-var myApp = new Framework7();
+var myApp = new Framework7({
+  modalTitle: 'Personal trainer',
+  init: false
+});
 
 // Export selectors engine
 var $$ = Framework7.$;
 
-// Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
+var view1 = myApp.addView('#view-1'); // Заглавная страница
+var view2 = myApp.addView('#view-2'); // Настройки
+var view3 = myApp.addView('#view-3'); // Клиенты
+var view4 = myApp.addView('#view-4'); // Ссылка на регистрацию
+var view5 = myApp.addView('#view-5'); // Группы упражнений
+var view7 = myApp.addView('#view-7'); // Управление списком упражнений
+var view8 = myApp.addView('#view-8'); // Страница настроек упражнения
+var view10 = myApp.addView('#view-10'); // Добавление клиента
+var view13 = myApp.addView('#view-13'); // Удаление клиентов из базы
+
+var server;
+db.open({
+  server: 'my-app',
+  version: 1,
+  schema: {
+    exerciseType: {
+      key: {
+        keyPath: 'id',
+        autoIncrement: true
+      },
+      indexes: {
+  			name: {
+  			  unique: true
+  			}
+      }
+    },
+    exercise: {
+      key: {
+        keyPath: 'id',
+        autoIncrement: true
+      },
+      indexes: {
+  			name: {
+  			  unique: true
+  			}
+      }
+    },
+    customers: {
+      key: {
+        keyPath: 'id',
+        autoIncrement: true
+      },
+      indexes: {
+  			name: {
+  			  unique: true
+  			}
+      }
+    }
+  }
+}).done(function(s) {
+  server = s;
 });
 
-// Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
+myApp.onPageBeforeInit('index-3', function (page) {
+  // Перед инициализацией страницы со списком клиентов, нужно подготовить этот список
+  alert('Pane 3 init');
+  alert(JSON.stringify(server))
+  server.customers.query('name')
+    .all()
+    .keys()
+    .distinct()
+    .execute()
+    .done(function(results) {
+      //alert(JSON.stringify(results));
+      var listCustomers = '';
+      results.forEach(function (val) {
+        //alert(val);
+        listCustomers += '<li class="item-content">';
+        listCustomers += '  <div class="item-inner">';
+        listCustomers += '    <div class="item-title">' + val + '</div>';
+        listCustomers += '  </div>';
+        listCustomers += '</li>';
+      });
+      document.getElementById("ulListCustomers").innerHTML = listCustomers;
+      /*var output="<ul>";
+      for (var i in data.exercise) {
+          output+="<li>" + data.exercise[i].name + "</li>";
+          alert(i);
+      }
+
+      output+="</ul>";
+      document.getElementById("placeholder").innerHTML=output;*/
     });
+  // Закрываем соединение с базой данных
+  //server.close();
 });
 
-// Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back-blue"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
-}
+myApp.onPageInit('index-2', function (page) {
+  alert('222');
+  console.log('2 page initialized');
+  console.log(page);
+});
+
+myApp.init();
+
 // Управляем видимостью кнопок Delete в настройках упражнений
 $$('.btn-delete-toggle').on('change', function(e) {
 	var collapse_content_selector = '#' + $$(this).attr('name');
@@ -63,3 +119,135 @@ $$('.confirm-ok-cancel').on('click', function () {
     );
 });
 
+//$(document).ready(function() {
+  
+  /*
+  // Пример чтения данных из файла json
+  $.getJSON('default/exercise.json', function(data) {
+      var output="<ul>";
+      for (var i in data.exercise) {
+          output+="<li>" + data.exercise[i].name + "</li>";
+          alert(i);
+      }
+
+      output+="</ul>";
+      document.getElementById("placeholder").innerHTML=output;
+  });*/
+  /*$.getJSON('default/exercise.json', function(data) {
+    // Получаем строку из файла json
+    var defaultExercisesString = JSON.stringify(data.exercise);
+    alert(defaultExercisesString);
+    // Вариант создания объекта из строки формата json:
+    //var defaultExercisesObject = JSON.parse(defaultExercisesString);
+    // Тогда можно обращаться с свойствам:
+    //alert(defaultExercisesObject[0].name);   
+    
+  });*/
+  // В целях тестирования и разработки каждый раз создаём базу с нуля. Очистим всё
+  /*var dbRequest = window.indexedDB.deleteDatabase("my-app");
+  dbRequest.onsuccess = function(evt) {
+    alert("DB is dropped");
+  };
+  dbRequest.onerror = function(evt){}*/
+    
+  
+// Эта функция должна отрабатывать при полной загрузке страницы
+$$(document).on('pageInit', function (e) {
+  alert("Start");
+  // Открываем базу данных
+  //var server;
+  db.open({
+    server: 'my-app',
+    version: 1,
+    schema: {
+      exerciseType: {
+        key: {
+          keyPath: 'id',
+          autoIncrement: true
+        },
+        indexes: {
+    			name: {
+    			  unique: true
+    			}
+        }
+      },
+      exercise: {
+        key: {
+          keyPath: 'id',
+          autoIncrement: true
+        },
+        indexes: {
+    			name: {
+    			  unique: true
+    			}
+        }
+      },
+      customers: {
+        key: {
+          keyPath: 'id',
+          autoIncrement: true
+        },
+        indexes: {
+    			name: {
+    			  unique: true
+    			}
+        }
+      }
+    }
+  }).done(function(s) {
+    server = s;
+    //alert(JSON.stringify(s));
+    //var defaultExercisesObject;
+    //var defaultExercisesString;
+    /*$.getJSON('default/exercises.json', function(data) {
+      //alert(JSON.stringify(data.exerciseType[0].exercises[0]));
+      for (var j in data.exerciseType) {
+        // Добавляем группы упражнений
+        server.exerciseType.add(data.exerciseType[j]).done(function(res){
+          //alert("Ok");
+        });
+        for (var i in data.exerciseType[j].exercises) {
+          //alert(data.exerciseType[0].exercises[i].name);
+          // Формируем базу упражнений по типам (типы заносим в отдельную таблицу)
+          
+          server.exercise.add({'name': data.exerciseType[j].exercises[i].name, 'type': j}).done(function(res){
+            //alert("Ok");
+          });
+        }
+      }
+    });*/
+    /*$.getJSON('default/customers.json', function(data) {
+      for (var i in data.customers) {
+        // Добавляем группы упражнений
+        server.customers.add(data.customers[i]).done(function(res){
+          //alert("Ok");
+        });
+      }
+    });*/
+    
+  });
+  server.exercise.query('name', 'Chin up')
+    .execute()
+    .done(function(results) {
+      alert(results);
+    });
+  // Закрываем соединение с базой данных
+  server.close();  
+  
+  var page = e.detail.page;
+  // Отдельный код для заглавной страницы
+  if (page.name === 'index-1') {
+      // We need to get count GET parameter from URL (about.html?count=10)
+      var count = page.query.count;
+      // Now we can generate some dummy list
+      var listHTML = '<ul>';
+      for (var i = 0; i < count; i++) {
+          listHTML += '<li>' + i + '</li>';
+      }
+      listHTML += '</ul>';
+      // And insert generated list to page content
+      $$(page.container).find('.page-content').append(listHTML);
+  }
+  
+  
+});
