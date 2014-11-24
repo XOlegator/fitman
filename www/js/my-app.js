1ea4378d3ba7f1815350126e8ca79330ced05b6a
XOlegator <subspam@mail.ru>
2014-11-24 18:49:07 +0400

    Добавлена библиотека для формирования графиков. На странице графика выполнения упражнения добавлен график с демо-данными.

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1606,5 +1606,29 @@ $('#aWorkStatistics').on('click', function() {
 // Функция срабатывает при нажатии кнопки Graph на странице работы с упражнением index-24
 $('#aWorkGraph').on('click', function() {
 	// Надо скрыть кнопку Save
-	$('#linkSaveWorkEx').hide(); 
+	$('#linkSaveWorkEx').hide();
+	var data = {
+  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  series: [
+    [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+    [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+  ]
+};
+
+var options = {
+  seriesBarDistance: 10
+};
+
+var responsiveOptions = [
+  ['screen and (max-width: 640px)', {
+    seriesBarDistance: 5,
+    axisX: {
+      labelInterpolationFnc: function (value) {
+        return value[0];
+      }
+    }
+  }]
+];
+
+new Chartist.Bar('.ct-chart', data, options, responsiveOptions);
 });
